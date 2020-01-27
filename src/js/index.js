76950bcd16c9b85070b1413def3d8f2a147810e8
shakeisback <iwan45u@gmail.com>
2020-01-27 12:32:42 +0200

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import './import/modules';
 import './import/components';
 
 function openDrop() {
-  this.$btn.classList.add('active');
+    this.$btn.classList.add('active');
 }
 /*
  * Chunks
@@ -11,14 +11,14 @@ function openDrop() {
 import { DropdownConstructor } from './import/chunks/dropdown/dropdown.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const dropDown_1 = new DropdownConstructor('[data-component="dropdown-1"]');
-  const dropDown_2 = new DropdownConstructor('[data-component="dropdown-2"]');
+    const dropDown_1 = new DropdownConstructor('[data-component="dropdown-1"]');
+    const dropDown_2 = new DropdownConstructor('[data-component="dropdown-2"]');
 
-  const _openDrop1 = openDrop.bind(dropDown_1);
+    const _openDrop1 = openDrop.bind(dropDown_1);
 
-  document.getElementById('js-lang').onclick = _openDrop1;
+    document.getElementById('js-lang').onclick = _openDrop1;
 
-  //   setTimeout(() => {
-  //     removeEvent.call(dropDown, 2);
-  //   }, 4000)
+    // setTimeout(() => {
+    //     removeEvent.call(dropDown, 2);
+    // }, 4000)
 });
